a1349ce521a3e79d01c3b661a9c01866617a3004
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-02-10 16:38:25 +0100

    Finished developing product microservice, testing ready

--- src/main/java/thirdwheel/product/dto/ProductRequest.java
+++ src/main/java/thirdwheel/product/dto/ProductRequest.java
@@ -6,9 +6,11 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductRequest {
+    private int id;
     private String name;
     private String description;
     private BigDecimal price;
--- src/main/java/thirdwheel/product/dto/ProductResponse.java
+++ src/main/java/thirdwheel/product/dto/ProductResponse.java
@@ -1,4 +1,20 @@
 package thirdwheel.product.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
 }
